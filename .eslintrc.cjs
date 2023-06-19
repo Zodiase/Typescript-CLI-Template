a4f80f0e5b7b1268dd6bed13a348ae710ec874fc
Xingchen Hong <hello@xc-h.com>
2023-06-18 18:29:03 -0700

    Add code style tooling.

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['eslint:recommended', 'prettier'],
+    env: {
+        node: true,
+        es2022: true,
+    },
+    root: true,
+    ignorePatterns: ['old-template'],
+};
