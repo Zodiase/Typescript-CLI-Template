bc306ec3a60b62271b09ca7619121802416f1850
Xingchen Hong <zodiase@gmail.com>
2023-06-22 14:35:36 -0700

    Only ensure project dir.

--- src/utility/ensureProjectDir.ts
+++ src/utility/ensureProjectDir.ts
@@ -1,9 +1,9 @@
-import { mkdirSync } from 'fs';
+import { ensureDirSync } from 'fs-extra';
 import Logger, { CommonLoggingKeys } from './Logger';
 
 const logger = new Logger(module.id);
 
 export default (projectDir: string) => {
     logger.debug('Creating project directory', { [CommonLoggingKeys.Path]: projectDir });
-    mkdirSync(projectDir);
+    ensureDirSync(projectDir);
 };
